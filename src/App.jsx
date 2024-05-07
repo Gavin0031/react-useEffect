1a782f8174f922e48fc35cc83212bcb5b980925b
Gavin LI <gavinli@Gavins-iMac.fios-router.home>
2024-05-07 17:22:44 -0400

    use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
-  const modal = useRef();
+  const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+  const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place)=>place.id === id));
+  //const modal = useRef();
+  const[modaIsOpen, setModalIsOpen] = useState(false);
+
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES, 
+        position.coords.latitude, 
+        position.coords.longitude);
+  
+        setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+  
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    //modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
+   // modal.current.close();
   }
 
   function handleSelectPlace(id) {
@@ -28,18 +48,29 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (storedIds.indexOf(id) === -1){
+      localStorage.setItem('selectedPlaces', JSON.stringify([id,...storedIds]))};
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
-  }
+    //modal.current.close();
+    setModalIsOpen(false);
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id)=> id !== selectedPlace.current))
+    )
+  };
+
+  
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modaIsOpen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -63,7 +94,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,37 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+// const Modal = forwardRef(function Modal({ children }, ref) {
+//   const dialog = useRef();
+
+//   useImperativeHandle(ref, () => {
+//     return {
+//       open: () => {
+//         dialog.current.showModal();
+//       },
+//       close: () => {
+//         dialog.current.close();
+//       },
+//     };
+//   });
+
+//   return createPortal(
+//     <dialog className="modal" ref={dialog}>
+//       {children}
+//     </dialog>,
+//     document.getElementById('modal')
+//   );
+// });
+function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(()=>{
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [])
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
@@ -21,6 +39,6 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     </dialog>,
     document.getElementById('modal')
   );
-});
+};
 
 export default Modal;
